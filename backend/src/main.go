7daa8d3514fec377b3d3f122487ae0bfe9103932
Kenneth López <3004966640101@ingenieria.usac.edu.gt>
2022-09-11 02:13:10 -0600

    Single output

--- backend/src/main.go
+++ backend/src/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("cat", "/proc/stat", "|", "grep", "cpu", "|", "tail", "-1", "|", "awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}'", "|", "awk '{print 100-$1}'")
-	out, err := cmd.CombinedOutput()
+	cmd := exec.Command("cat /proc/stat | grep cpu | tail -1 | awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}' | awk '{print 100-$1}'")
+	out, err := cmd.Output()
 	if err != nil {
 		fmt.Println(err)
 	} else {
