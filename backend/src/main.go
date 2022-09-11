cf5838cfdaeab8379370a6e5d1e751a3002743c7
Kenneth López <3004966640101@ingenieria.usac.edu.gt>
2022-09-11 02:15:37 -0600

    CombinedOutput

--- backend/src/main.go
+++ backend/src/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	cmd := exec.Command("cat /proc/stat | grep cpu | tail -1 | awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}' | awk '{print 100-$1}'")
-	out, err := cmd.Output()
+	out, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println(err)
 	} else {
