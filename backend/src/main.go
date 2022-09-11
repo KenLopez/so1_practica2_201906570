e3d64660ec173d1468f240db3087878807b3274c
Kenneth López <3004966640101@ingenieria.usac.edu.gt>
2022-09-11 02:09:50 -0600

    Fix comando

--- backend/src/main.go
+++ backend/src/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("echo", "cat", "/proc/stat", "|", "grep", "cpu", "|", "tail", "-1", "|", "awk '{($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}'", "|", "awk '{100-$1}'")
+	cmd := exec.Command("cat", "/proc/stat", "|", "grep", "cpu", "|", "tail", "-1", "|", "awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}'", "|", "awk '{print 100-$1}'")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println(err)
